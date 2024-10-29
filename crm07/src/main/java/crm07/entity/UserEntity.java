8d9d0e1ee4c318b967e5395e73a17db1721f04d7
Minh Dinh <minhdinh1552@gmail.com>
2024-10-29 14:52:39 +0700

    add crm07

---
+++ crm07/src/main/java/crm07/entity/UserEntity.java
@@ -0,0 +1,60 @@
+package crm07.entity;
+
+public class UserEntity {
+	private int id;
+	private String email;
+	private String password;
+	private String fullname;
+	private String userAvatar;
+	private String roleName;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getFullname() {
+		return fullname;
+	}
+	
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	
+	public String getUserAvatar() {
+		return userAvatar;
+	}
+	
+	public void setUserAvatar(String userAvatar) {
+		this.userAvatar = userAvatar;
+	}
+	
+	public String getRoleName() {
+		return roleName;
+	}
+	
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+	
+}
---
+++ crm07/src/main/java/crm07/repository/UserRepository.java
@@ -0,0 +1,120 @@
+package crm07.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import config.MysqlConfig;
+import crm07.entity.UserEntity;
+
+public class UserRepository {
+	public ArrayList<UserEntity> findByEmailAndPassword(String email, String password) {
+		ArrayList<UserEntity> userList = new ArrayList<UserEntity>();
+		
+		Connection conn = MysqlConfig.getConnection();
+		String query = "SELECT * FROM users u JOIN roles r ON u.role_id = r.id WHERE u.email = ? AND u.password = ?";
+		
+		try {
+			PreparedStatement prepStatement = conn.prepareStatement(query);
+			prepStatement.setString(1, email);
+			prepStatement.setString(2, password);
+			ResultSet res = prepStatement.executeQuery();
+			
+			
+			while (res.next()) {
+				UserEntity user = new UserEntity();
+				user.setId(res.getInt("id"));
+				user.setEmail(res.getString("email"));
+				user.setRoleName(res.getString("name"));
+				
+				userList.add(user);
+				
+			}
+			
+		} 
+		catch(Exception e) {
+			System.out.println("Find user by email and password error " + e.getMessage());
+		}
+		
+		
+		
+		
+		return userList;
+	}
+	
+	public ArrayList<UserEntity> findAll() {
+		ArrayList<UserEntity> userList = new ArrayList<UserEntity>();
+
+		Connection conn = MysqlConfig.getConnection();
+		String query = "SELECT u.id, u.fullname, u.email, r.name FROM users u JOIN roles r ON u.role_id = r.id";
+		
+		try {
+			PreparedStatement prepStatement = conn.prepareStatement(query);
+			ResultSet res = prepStatement.executeQuery();
+			
+			
+			while (res.next()) {
+				UserEntity user = new UserEntity();
+				user.setId(res.getInt("id"));
+				user.setEmail(res.getString("email"));
+				user.setRoleName(res.getString("name"));
+				user.setFullname(res.getString("fullname"));
+				
+				userList.add(user);
+				
+			}
+			
+		} 
+		catch(Exception e) {
+			System.out.println("Find user by email and password error " + e.getMessage());
+		}
+		
+		
+		
+		return userList;
+	}
+	
+	
+	public int deleteById(int id) {
+		Connection conn = MysqlConfig.getConnection();
+		String query = "DELETE FROM users u WHERE u.id = ?";
+		int rowDeleted = 0;
+		
+		try {
+			PreparedStatement prepStatement = conn.prepareStatement(query);
+			prepStatement.setInt(1, id);
+			
+			rowDeleted = prepStatement.executeUpdate();
+
+		} 
+		catch(Exception e) {
+			System.out.println("Delete user by id error " + e.getMessage());
+		}
+		
+		return rowDeleted;
+	}
+	
+	public int insertUser(String name, String email, String password, int roleId) {
+		Connection conn = MysqlConfig.getConnection();
+		String query = "INSERT INTO users (email, password, fullname, role_id) VALUES (?, ?, ?, ?)";
+		int rowInserted = 0;
+		
+		try {
+			PreparedStatement prepStatement = conn.prepareStatement(query);
+			prepStatement.setString(1, email);
+			prepStatement.setString(2, password);
+			prepStatement.setString(3, name);
+			prepStatement.setInt(4, roleId);
+			
+			rowInserted = prepStatement.executeUpdate();
+
+		} 
+		catch(Exception e) {
+			System.out.println("insert user error " + e.getMessage());
+		}
+		
+		return rowInserted;
+		
+	}
+}
